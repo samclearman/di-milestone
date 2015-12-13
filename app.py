75599d16eced6fb0648efb982ab6d32069182f46
Sam Clearman <sam@samclearman.com>
2015-12-13 10:40:47 -0500

    stocker ticker

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template, request, redirect
+from pprint import pformat
+
+import pandas as pd
+
+from bokeh.plotting import figure
+from bokeh.embed import components
+
+import requests
+
+import datetime
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
 def main():
@@ -9,6 +20,21 @@ def main():
 @app.route('/index')
 def index():
   return render_template('index.html')
+
+@app.route('/stock')
+def stock():
+  start_d = str(datetime.date.today() - datetime.timedelta(days=30))
+  end_d = str(datetime.date.today())
+  ticker = request.args.get('ticker')
+  r = requests.get('https://www.quandl.com/api/v3/datasets/WIKI/{}.json?&start_date={}&end_date={}&column_index=4'.format(ticker,start_d,end_d))
+  data = r.json()['dataset']['data']
+  df = pd.DataFrame(data)
+  p = figure(title="{} chart".format(ticker), x_axis_type='datetime')
+  p.circle(pd.to_datetime(df[0]),df[1])
+  script,div = components(p)
+  # raw = r.text()
+  
+  return render_template('stock.html',raw=data,script=script,div=div,ticker=ticker)
 
 if __name__ == '__main__':
   app.run(port=33507)
